6f7e54cedcca260fdf8953ae246aa348a03cf5c0
Андрей Когут <joker320kingkobra@mail.ru>
2021-05-31 21:17:00 +1000

    Добавьте файлы проекта.

---
+++ Cosmostars/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Cosmostars
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+
+        Random rand = new Random();
+
+        int score = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)//что мы делаем за один тик времени
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Down(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Up(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
